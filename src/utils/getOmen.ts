e5eec6d73e1cbb25dfb1156ae435e41211762ed7
wooooooood <13pft13@gmail.com>
2023-08-12 22:32:42 +0900

    fix. omen

--- src/utils/getOmen.ts
+++ src/utils/getOmen.ts
@@ -1,5 +1,12 @@
+import { STONES } from "../constants/stone";
+
 export const getOmen = (question: string) => {
-  const refinedQuestion = question.split('').reduce((prev, cur) => prev += cur.charCodeAt(0),0);
+  const refinedQuestion = question
+    .split("")
+    .reduce(
+      (prev, cur) => (prev += Math.floor(Math.random() * cur.charCodeAt(0))),
+      0,
+    );
   const elixer = 2;
-  return (refinedQuestion % elixer); 
-}
+  return STONES[refinedQuestion % elixer];
+};
