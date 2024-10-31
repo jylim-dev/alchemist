cb6bb3e90a550d9a8e17f4b814ded42f08ac2380
jylim-dev <jylim.dev@gmail.com>
2024-10-31 00:18:34 -0700

    use sass and english

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,37 +1,38 @@
-import { useState } from "react";
-import { getOmen } from "../utils/getOmen";
+import { useState } from 'react';
+import { getOmen } from '../utils/getOmen';
+import './question.scss';
 
 const Question = () => {
-  const [q, setQ] = useState("");
+  const [q, setQ] = useState('');
   const [stone, setStone] = useState<string | null>(null);
   const pickStone = () => {
-    setStone(() => "");
+    setStone(() => '');
     setTimeout(() => {
       const omen = getOmen(q);
-      console.log("omen: ", omen);
+      console.log('omen: ', omen);
       setStone(() => omen);
     }, 1500);
   };
   return (
-    <div>
+    <div className="question-container">
+      <h1>What is it that troubles your heart?</h1>
+      <p>
+        The question must be one the heart can answer with a simple yes or no.
+      </p>
       <div>
-        <h1>무엇이 궁금한가요?</h1>
-        <p>예/아니오로 대답할 수 있는 질문이어야 합니다</p>
         <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+        <button className="" onClick={pickStone}>
+          Seek the truth in the prophecy
+        </button>
       </div>
-      <button className="" onClick={pickStone}>
-        예언 확인하기
-      </button>
-      <div>
-        <p>결과:</p>
-        {stone === null ? (
-          <></>
-        ) : stone === "" ? (
-          <p>뒤적뒤적..</p>
-        ) : (
-          <p>{stone === "Urim" ? "⚪️ No" : "⚫️ Yes"}</p>
-        )}
-      </div>
+      <p>The truth that lies at the journey's end:</p>
+      {stone === null ? (
+        <></>
+      ) : stone === '' ? (
+        <p>Searching, sifting, in the quiet dance of discovery..</p>
+      ) : (
+        <p>{stone === 'Urim' ? '⚪️ No' : '⚫️ Yes'}</p>
+      )}
     </div>
   );
 };
